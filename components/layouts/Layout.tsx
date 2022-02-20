de6affc2b2c7aead93165967569b6e5f19510ccb
abeshi03 <k_913411@yahoo.co.jp>
2022-02-20 23:44:32 +0900

    矢印アニメーションを追加

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,10 +1,37 @@
 import { VFC, memo, ReactNode } from "react";
+import Link from "next/link";
 import styles from "./Layout.module.scss";
 
 
 type Props = {
   children: ReactNode;
 }
+
+type Navigation = {
+  icon?: JSX.Element;
+  pageName: string;
+  path: string;
+}
+
+const navigations: Navigation[] = [
+  {
+    pageName: "トップ",
+    path: "/"
+  },
+  {
+    pageName: "ページ2",
+    path: "#"
+  },
+  {
+    pageName: "ページ3",
+    path: "#"
+  },
+  {
+    pageName: "ページ4",
+    path: "#"
+  },
+
+]
 
 export const Layout: VFC<Props> = memo((props) => {
 
@@ -13,7 +40,27 @@ export const Layout: VFC<Props> = memo((props) => {
   return (
     <div className={styles.root}>
 
-      {children}
+      <aside className={styles.sidebar}>
+        <h1 className={styles.logo}>ロゴが入ります</h1>
+        {navigations.map((navigation) => (
+          <Link href={navigation.path} key={navigation.path}>
+            <a>
+              <p className={styles.pageName}>{ navigation.pageName }</p>
+            </a>
+          </Link>
+        ))}
+      </aside>
+
+      <main className={styles.mainContent}>
+        <div className={styles.header}>
+          <div className={styles.hamburger} role="button">
+            <span className={styles.border}></span>
+            <span className={styles.border}></span>
+            <span className={styles.border}></span>
+          </div>
+        </div>
+        {children}
+      </main>
     </div>
   )
 })
