9113c6cabba6e3ee0f8a63e8bca4799041e82626
abeshi03 <k_913411@yahoo.co.jp>
2022-02-26 18:42:43 +0900

    アニメーション実装追加

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import {VFC, memo, ReactNode, useState} from "react";
 import Link from "next/link";
 import styles from "./Layout.module.scss";
+import { HomeIcon } from "../../styles/Home";
 
 
 type Props = {
@@ -16,19 +17,23 @@ type Navigation = {
 const navigations: Navigation[] = [
   {
     pageName: "トップ",
-    path: "/"
+    path: "/",
+    icon: <HomeIcon className={styles.icon}/>
   },
   {
     pageName: "ページ2",
-    path: "#"
+    path: "#",
+    icon: <HomeIcon className={styles.icon}/>
   },
   {
     pageName: "ページ3",
-    path: "#"
+    path: "#",
+    icon: <HomeIcon className={styles.icon}/>
   },
   {
     pageName: "ページ4",
-    path: "#"
+    path: "#",
+    icon: <HomeIcon className={styles.icon}/>
   },
 
 ]
@@ -37,29 +42,46 @@ export const Layout: VFC<Props> = memo((props) => {
 
   const { children } = props;
 
-  const [ menuClose, setMenuClose ] = useState(false);
+  const [ menuOpen, setMenuOpen ] = useState(false);
 
   return (
     <div className={styles.root}>
 
-      <aside className={styles.sidebar}>
-        <div className={styles.hamburger} role="button" onClick={() => setMenuClose(!menuClose)}>
-          <span className={styles.border}></span>
-          <span className={styles.border}></span>
-          <span className={styles.border}></span>
-        </div>
-        {navigations.map((navigation) => (
-          <Link href={navigation.path} key={navigation.path}>
-            <a className={styles.flexContainer}>
-              <div className={styles.icon}>○</div>
-              <p
-                className={styles.pageName}
-                style={{display: menuClose ? "none" : "block"}}
-              >{ navigation.pageName }</p>
-            </a>
-          </Link>
-        ))}
-      </aside>
+      { menuOpen ? (
+        <aside className={styles.sidebar} style={{ width: "200px" }}>
+          <div className={styles.hamburger} role="button" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className={styles.border}></span>
+            <span className={styles.border}></span>
+            <span className={styles.border}></span>
+          </div>
+          {navigations.map((navigation) => (
+            <Link href={navigation.path} key={navigation.path}>
+              <a className={styles.flexContainer}>
+                {navigation.icon}
+                <p
+                  className={styles.pageName}
+                >ほげ</p>
+              </a>
+            </Link>
+          ))}
+        </aside>
+      ) : (
+        <aside className={styles.sidebar} style={{ width: "60px" }}>
+          <div className={styles.hamburger} role="button" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className={styles.border}></span>
+            <span className={styles.border}></span>
+            <span className={styles.border}></span>
+          </div>
+          {navigations.map((navigation) => (
+            <Link href={navigation.path} key={navigation.path}>
+              <a className={styles.flexContainer}>
+                {navigation.icon}
+              </a>
+            </Link>
+          ))}
+        </aside>
+      )}
+
 
       <main className={styles.mainContent}>
         {children}
